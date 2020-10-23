cf0a23282a480e5441389289f20ea083faf5242c
Taylon <taylondavila@gmail.com>
2020-10-23 17:29:05 -0300

    implementando tela de login

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -26,7 +27,26 @@ $app->get('/admin', function() {
 
 });
 
+$app->get('/admin/login', function(){
 
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false,
+
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+
+
+});
 
 $app->run();
 
